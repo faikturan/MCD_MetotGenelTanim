4f3a2d5d5bc0611c0db2ed1aa032302d09f9f449
Faik Turan <turanfaik@hotmail.com>
2021-11-23 15:54:14 +0300

    Add project files.

---
+++ MCD_MetotGenelTanim/Ogrenci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_MetotGenelTanim
+{
+    public class Ogrenci
+    {
+        public void selamlaOgrenci()
+        {
+            Console.WriteLine("Merhaba Öğrenci");
+        }
+    }
+    /* [ErişimBelirleyicisi] <GeriDonusDegeri> MetotAdi(Parametreler)
+     * {
+     *      //Metot için gereken kodlar
+     * }
+     * 
+     * 
+     * 
+     * 
+    *
+    */
+}
